2f0f72f87c87ec0c1c4046cf2bbf49dd12159076
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-02-09 05:03:37 +0100

    button fixed

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ var rootElement = document.documentElement
 function handleScroll() {
   // do something on scroll
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal ) > 1) {
+  if ((rootElement.scrollTop / scrollTotal ) > 0) {
     //show button
-    scrollToTopBtn.style.display = "none"
+    scrollToTopBtn.style.display = "block"
   } else {
     //hide button
-    scrollToTopBtn.style.display = "block"
+
+    scrollToTopBtn.style.display = "none"
   }
 }
 
